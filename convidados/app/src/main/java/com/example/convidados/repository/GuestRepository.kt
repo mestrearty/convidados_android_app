75852f1801417909d1f513bcbf94d53c45248951
Pedro Ventura <phventurarodrigues@gmail.com>
2023-08-15 18:54:03 -0300

    Insert, Update and Delete - Guest Methods

--- convidados/app/src/main/java/com/example/convidados/repository/GuestRepository.kt
+++ convidados/app/src/main/java/com/example/convidados/repository/GuestRepository.kt
@@ -40,8 +40,37 @@ class GuestRepository private constructor(context: Context) {
         }
     }
 
-    fun update() {
+    fun update(guest: GuestModel): Boolean {
+        try {
+            val db = guestDataBase.writableDatabase
+            val presence = if (guest.presence) 1 else 0
 
+            val values = ContentValues()
+            values.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.nome)
+            values.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, presence)
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ? "
+            val args = arrayOf(guest.id.toString())
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, values, selection, args)
+
+            return true
+        } catch (error: Exception) {
+            return false
+        }
+
+    }  fun delete(id: Int): Boolean {
+        try {
+            val db = guestDataBase.writableDatabase
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ? "
+            val args = arrayOf(id.toString())
+
+            db.delete(DataBaseConstants.GUEST.TABLE_NAME, selection, args)
+
+            return true
+        } catch (error: Exception) {
+            return false
+        }
     }
 
 }
